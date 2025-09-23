284f1fb482b5397cabd249825322cf7a562c3e84
idjfjsna0 <nataliya.aleksei@gmail.com>
2025-09-23 18:30:57 +0300

    actual version 23.09.25

--- data_loader.py
+++ data_loader.py
@@ -1,9 +1,19 @@
 import pandas as pd
 
-file_id = "1sStrkLT-LQ3sIPJD-rPY0LNCCk5l1lbg"  # ID файла на Google Drive
-file_url = f"https://drive.google.com/uc?id={file_id}"
+FILE_ID = "1sStrkLT-LQ3sIPJD-rPY0LNCCk5l1lbg"  # ID
+file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 
-raw_data = pd.read_csv(file_url, skiprows=18, delimiter=";", encoding="utf-8")
+try:
+    raw_data = pd.read_csv(file_url, on_bad_lines="skip", encoding="utf-8")
 
-raw_data.head(10)  # выводим на экран первые 10 строк для проверки
-# https://drive.google.com/file/d/1sStrkLT-LQ3sIPJD-rPY0LNCCk5l1lbg/view?usp=sharing
+    print("File succesfully downloaded.")
+    print(f"Downloaded {raw_data.shape[0]} rows, {raw_data.shape[1]} columns")
+
+    if raw_data.shape[0] > 0:
+        print("\nFirst 10 rows of dataset:")
+        print(raw_data.head(10))
+    else:
+        print("No data or wrong format")
+
+except Exception as e:
+    print(f"Error: {e}")
