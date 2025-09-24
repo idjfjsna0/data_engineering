464c4aabef675b4471f2bb7b20d532f8af9da0b4
idjfjsna0 <nataliya.aleksei@gmail.com>
2025-09-24 11:24:21 +0300

    24.09.25

--- data_loader.py
+++ data_loader.py
@@ -4,7 +4,7 @@ FILE_ID = "1sStrkLT-LQ3sIPJD-rPY0LNCCk5l1lbg"  # ID
 file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 
 try:
-    raw_data = pd.read_csv(file_url, on_bad_lines="skip", encoding="utf-8")
+    raw_data = pd.read_csv(file_url, on_bad_lines="skip", skiprows=17, encoding="utf-8")
 
     print("File succesfully downloaded.")
     print(f"Downloaded {raw_data.shape[0]} rows, {raw_data.shape[1]} columns")
