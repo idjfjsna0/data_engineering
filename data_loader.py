d81f05dcbbf4af56bf933bd782466b0a18e06a72
idjfjsna0 <nataliya.aleksei@gmail.com>
2025-09-29 21:30:41 +0300

    h/w #3

--- data_loader.py
+++ data_loader.py
@@ -4,12 +4,20 @@ FILE_ID = "1sStrkLT-LQ3sIPJD-rPY0LNCCk5l1lbg"  # ID
 file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 
 try:
-    raw_data = pd.read_csv(file_url, on_bad_lines="skip", skiprows=17, encoding="utf-8")
+    raw_data = pd.read_csv(
+        file_url, on_bad_lines="skip", skiprows=17, encoding="utf-8", delimiter=";"
+    )
 
-    print("File succesfully downloaded.")
+    print("File successfully downloaded.")
     print(f"Downloaded {raw_data.shape[0]} rows, {raw_data.shape[1]} columns")
 
     if raw_data.shape[0] > 0:
+        raw_data = raw_data.dropna(how="all")  # Delete all-NaN rows
+
+        for col in raw_data.columns:
+            raw_data[col] = pd.to_numeric(raw_data[col], errors="coerce")  #
+
+        raw_data = raw_data.dropna()  # Delete rows, which contains NaN
         print("\nFirst 10 rows of dataset:")
         print(raw_data.head(10))
     else:
@@ -17,3 +25,5 @@ try:
 
 except Exception as e:
     print(f"Error: {e}")
+
+raw_data.to_csv("trying_de/spectrum_data.csv", index=False)
