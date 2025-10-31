3a022c6b4281662bbd92983bb5ae4937576d1298
idjfjsna0 <nataliya.aleksei@gmail.com>
2025-10-31 23:57:34 +0300

    hw 8

--- trying_de/data_loader.py
+++ trying_de/data_loader.py
@@ -1,9 +1,32 @@
 import pandas as pd
 
-file_id = "1sStrkLT-LQ3sIPJD-rPY0LNCCk5l1lbg"  # ID файла на Google Drive
-file_url = f"https://drive.google.com/uc?id={file_id}"
+FILE_ID = "1sStrkLT-LQ3sIPJD-rPY0LNCCk5l1lbg"  # ID
+file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 
-raw_data = pd.read_csv(file_url, skiprows=18, delimiter=";", encoding="utf-8")
-print(raw_data[0])
-raw_data.head(10)  # выводим на экран первые 10 строк для проверки
-# https://drive.google.com/file/d/1sStrkLT-LQ3sIPJD-rPY0LNCCk5l1lbg/view?usp=sharing
+try:
+    raw_data = pd.read_csv(
+        file_url, on_bad_lines="skip", skiprows=17, encoding="utf-8", delimiter=";"
+    )
+
+    print("File successfully downloaded.")
+    print(f"Downloaded {raw_data.shape[0]} rows, {raw_data.shape[1]} columns")
+
+    if raw_data.shape[0] > 0:
+        raw_data = raw_data.dropna(how="all")  # Delete all-NaN rows
+
+        for col in raw_data.columns:
+            raw_data[col] = pd.to_numeric(raw_data[col], errors="coerce")
+
+        raw_data = raw_data.dropna()  # Delete rows, which contains NaN
+
+        print("\nFirst 10 rows of dataset:")
+        print(raw_data.head(10))
+        print("Data types of columns:")
+        print(raw_data.dtypes)
+    else:
+        print("No data or wrong format")
+
+except Exception as e:
+    print(f"Error: {e}")
+
+raw_data.to_csv("trying_de/spectrum_data.csv", index=False)
